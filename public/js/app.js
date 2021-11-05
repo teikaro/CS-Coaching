a9ec706fff57604dfd3e6dfe2d38a5d69d73e389
Corentin <a@a.a>
2021-11-05 18:04:14 +0100

    nouveau départ

--- public/js/app.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    // Rend temporaire les div alerts (messages)
-    if ($('div.alert')) {
-        $('div.alert').delay(3500).fadeOut(300, function() {
-            $('div.alert').remove();
-        });
-    }
-});
